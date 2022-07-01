febc78a5a241d6451d94a9355c0d4e652c97d358
Mike DiDomizio <mikedidomizio@gmail.com>
2022-07-01 10:07:27 -0400

    Separate security groups

--- security_groups.tf
+++ security_groups.tf
@@ -19,8 +19,33 @@ resource "aws_security_group" "security_group" {
   }
 
   # allow EC2 to connect to RDS, not sure if this is correct
+#  ingress {
+#    from_port   = 80
+#    to_port     = 3306
+#    protocol    = "tcp"
+#    cidr_blocks = [aws_vpc.vpc.cidr_block] # todo not sure if this is right
+#  }
+
+  # allow everything that is required since terraform removes it automatically (https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Security Group ${var.tag}"
+  }
+}
+
+resource "aws_security_group" "db_security_group" {
+  name   = "Security Group DB ${var.tag}"
+  vpc_id = aws_vpc.vpc.id
+
+  # allow EC2 to connect to RDS, not sure if this is correct
   ingress {
-    from_port   = 80
+    from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = [aws_vpc.vpc.cidr_block] # todo not sure if this is right
@@ -35,6 +60,6 @@ resource "aws_security_group" "security_group" {
   }
 
   tags = {
-    Name = "Security Group ${var.tag}"
+    Name = "Security Group DB ${var.tag}"
   }
 }
